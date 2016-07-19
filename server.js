80f67abeb0c28e35edbc7d0518ee052fe7cfb73b
Gin <vienlyt@gmail.com>
2016-07-19 10:22:14 -0700

    done

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const net = require('net');
+const ClientPool = require(__dirname + '/lib/ClientPool');
+
+const testPool = new ClientPool();
+const server = net.createServer();
+
+var port = 3000;
+
+server.on('connection', (socket) => {
+  testPool.ee.emit('register', socket);
+});
+
+server.listen(port, () => {
+  console.log('server running on port ' + port);
+});
